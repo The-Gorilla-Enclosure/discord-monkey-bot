41bff5895a771b78f455e258f8b7b2a42d118368
arshtsingh <arsh_singh@live.com>
2022-07-26 20:24:17 -0500

    WIP monke new command

--- README.rst
+++ README.rst
@@ -9,3 +9,9 @@ Poetry Commands
 ---------------
 
 poetry run bot
+
+
+TODO:
+------
+- use tenor api to get back a url for a monkey gif https://developers.google.com/tenor/guides/quickstart
+- finish implementing monkey new
--- src/service.py
+++ src/service.py
@@ -1,5 +1,25 @@
+import os    
+import discord
+from dotenv import load_dotenv
+
 def main():
-    print("hello world")
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    client = discord.Client()
+    @client.event
+    async def on_ready():
+        print(f'{client.user} has connected to Discord!')
+        
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        if '!monke' in message.content.lower():
+            if 'new' in message.content.lower():
+                await message.channel.send('https://tenor.com/view/cool-shades-on-wink-banana-gif-15443150')
+            else:
+                await message.channel.send('gorilla need mo bananas')
+    client.run(TOKEN)
 
 if __name__ == "__main__":
     main()
